08ab1e9230c757d85ddd01ea316d5e8194a527cd
Mauricio Rissi <mauriciorissi@gmail.com>
2017-06-04 20:43:43 -0300

    Corrigido Bug da barra de scroll

--- src/views/createEvent.js
+++ src/views/createEvent.js
@@ -57,10 +57,21 @@ class CreateEventComponent extends React.Component {
                                            title="Título do Evento" rel="txtTooltip"/>
                                 </div>
                             </div>
+                            <div className="col-md-10 col-md-offset-1">
+                                <div className="form-group">
+                                    <h3>SubTítulo:</h3>
+                                    <input tabIndex="2" type="text" className="form-control input-lg"
+                                           placeholder="SubTítulo"
+                                           aria-describedby="basic-addon1" name="subtitle" id="eventSubTitle"
+                                           maxLength="30"
+                                           data-toggle="tooltip" data-placement="left"
+                                           title="SubTítulo" rel="txtTooltip"/>
+                                </div>
+                            </div>
                             <div className="col-md-5 col-md-offset-1">
                                 <div className="form-group">
                                     <h3>Descrição do Evento:</h3>
-                                    <textarea tabIndex="2" className="form-control" placeholder="Descrição do Evento"
+                                    <textarea tabIndex="3" className="form-control" placeholder="Descrição do Evento"
                                               aria-describedby="basic-addon1" name="description" id="eventDescription"
                                               maxLength="255" rows="20"
                                               data-toggle="tooltip" data-placement="left"
@@ -69,40 +80,28 @@ class CreateEventComponent extends React.Component {
                             </div>
                             <div className="col-md-5">
                                 <div className="form-group">
+
                                     <AddressComponent/>
+                                    
                                     <div className="form-group">
                                         <br/>
-                                        <h3>Foto de divulgação:</h3>
-                                        <div className="input-group file-preview">
-                                            <input placeholder="" type="text"
-                                                   className="form-control file-preview-filename"
-                                                   disabled="disabled" id="fileName"/>
-                                            <span className="input-group-btn">
-                                                <button type="button" className="btn btn-default file-preview-clear"
-                                                        style={{display: 'none'}}> <span
-                                                    className="glyphicon glyphicon-remove"></span> Limpar </button>
-                                                <div className="btn btn-default file-preview-input">
-                                        <span className="glyphicon glyphicon-folder-open"></span>
-                                        <span className="file-preview-input-title">Escolher Imagem</span>
-                                        <input tabIndex="7" type="file" accept="image/*" name="input-file-preview"/>
-                                    </div>
-                                    <button type="button" className="btn btn-labeled btn-primary"> <span
-                                        className="btn-label"><i
-                                        className="glyphicon glyphicon-upload"></i> </span>Upload</button>
-                                </span>
-                                        </div>
+                                        <h3>Data e Hora:</h3>
+                                        <input tabIndex="8" type="text" className="form-control"
+                                               aria-describedby="basic-addon1" name="house" id="dateTime"
+                                               data-toggle="tooltip" data-placement="left"
+                                               title="Data e Hora do Evento" rel="txtTooltip"/>
                                     </div>
 
                                     <br/><br/><br/><br/>
 
                                     <div className="input-group pull-left" id="canceldiv">
-                                        <input tabIndex="8" type="reset" value="Cancelar"
+                                        <input tabIndex="9" type="reset" value="Cancelar"
                                                className="btn btn-lg btn-danger btn-submit"
                                                id="cancel-btn"/>
                                     </div>
 
                                     <div className="input-group pull-right" id="submitdiv">
-                                        <input tabIndex="9" type="submit" value="Cadastrar Evento"
+                                        <input tabIndex="10" type="submit" value="Cadastrar Evento"
                                                className="btn btn-lg btn-success btn-submit"
                                                id="submit-form"/>
                                     </div>
